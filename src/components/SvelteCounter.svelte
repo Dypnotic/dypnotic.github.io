14e5b9da91810d46c2a47c937034ca3828b34ec2
Jens Cherukad <jc@decid.io>
2022-04-05 18:24:19 +0200

    Switch to Astro with Svelte [app created]

---
+++ src/components/SvelteCounter.svelte
@@ -0,0 +1,23 @@
+<script>
+  let count = 0;
+
+  function add() {
+    count += 1;
+  }
+
+  function subtract() {
+    count -= 1;
+  }
+</script>
+
+<div id="svelte" class="counter test">
+  <button on:click={subtract}>-</button>
+  <pre>{ count }</pre>
+  <button on:click={add}>+</button>
+</div>
+
+<style>
+	.test {
+		background-color: var(--color-test);
+	}
+</style>
