fc9f4ac680ec3c31a7718b1aad9cb95551ec2a4a
Dypnotic <jenscherukad@gmail.com>
2025-04-01 16:22:32 +0200

    Transfer about me to svelte

---
+++ src/routes/i/+page.svelte
@@ -0,0 +1,48 @@
+	<h1 lang=ja>
+		<p class="translation" lang=en>This is me</p>
+		こちらは俺だ
+	</h1>
+	<p>Born in Vienna, Austria to Indian immigrants. I enjoy design, coding and too many other things.</p>
+
+	<h2 lang=ja>
+		<p class="translation" lang=en>What's a Dypnotic?</p>
+		ヂィプノチィックとは何か
+	</h2>
+	<p>
+		On 13 November 2000, the French electronic music duo <em>Daft Punk</em> released the lead single, <cite>One More
+			Time</cite>, from their second studio album, <cite>Discovery</cite>. It features lyrics and vocals by Texas house
+		DJ
+		and singer Romanthony.
+	</p>
+	<p>
+		Two years later, the French duo released the remix album <cite>Daft Club</cite>. It features an unplugged version of
+		the hit track, with lyrics that were slightly altered, perhaps even improvised, especially in the last verse:
+	</p>
+	<figure>
+		<blockquote cite="https://genius.com/Daft-punk-one-more-time-romanthonys-unplugged-lyrics">
+			Before the machines take over
+			And the system gets us in
+			Just be a lover one more time
+			Now and against
+			Daft Punk
+			Robotic, <strong>Dypnotic</strong>, Romanthony
+			Doin' it one more time
+		</blockquote>
+		<figcaption>
+			— Romanthony, <cite>One More Time (Romanthony&rsquo;s Unplugged)</cite>
+		</figcaption>
+	</figure>
+	<p>Teenage me thought this was <ruby lang=ja>めっちゃ<rt lang=en>super</rt></ruby> <ruby lang=ja>かっこいい<rt lang=en>cool
+			</rt>
+		</ruby> (I still do). It was
+		obscure, unique and meant nothing. So I made it my own.
+	<h2 lang=ja>
+		<p class="translation" lang=en>Why Japanese?</p>
+		なぜ日本語なのか
+	</h2>
+	<p>
+		At the time of writing, I am currently learning Japanese. As a huge gaming and anime enthusiast I want to be
+		able to watch and understand Japanese media without having a layer of translation. Using Japanese in titles and
+		occasionally elsewhere is my way of having fun with it.
+	</p>
+
